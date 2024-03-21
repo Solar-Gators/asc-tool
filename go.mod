f7a1d9a3f2279f9adbc291802b6d6c6b5e74fdbf
hhhtylerw <hhhtylerw@protonmail.com>
2024-03-20 23:40:35 -0400

    read gpx file for routes

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module asc-simulation
 
 go 1.21.5
+
+require (
+	github.com/tkrajina/gpxgo v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- physics-sim.go
+++ physics-sim.go
@@ -1,8 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/tkrajina/gpxgo/gpx"
+)
 
 // physics sim should be main program
 func main() {
-	fmt.Println(CallApi("maps"))
+	// fmt.Println(CallApi("maps"))
+
+	file, err := os.Open("ASC-2022-Reference-Route-V2.gpx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// Get the file size
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Read the file into a byte slice
+	bs := make([]byte, stat.Size())
+	_, err = bufio.NewReader(file).Read(bs)
+	if err != nil && err != io.EOF {
+		fmt.Println(err)
+		return
+	}
+
+	gpxFile, err := gpx.ParseBytes(bs)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(len(gpxFile.Routes))
+
+	for _, route := range gpxFile.Routes {
+		fmt.Println(route.Name)
+		fmt.Println(len(route.Points))
+		for _, point := range route.Points {
+			if point.Name != "" {
+				fmt.Println(point.Name)
+			}
+		}
+		fmt.Println()
+	}
 }
