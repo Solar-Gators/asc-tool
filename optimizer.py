0a935a0c2ea1cc6a2d3477b6cab90884a1d39136
Jack Schedel <jackschedel@gmail.com>
2024-03-28 20:00:38 -0400

    infinitely solving but stuck at bad chi

--- optimizer.py
+++ optimizer.py
@@ -8,7 +8,7 @@ from mystic.solvers import *
 from mystic.strategy import *
 from mystic.termination import *
 
-CALLS_BETWEEN_IMAGE = 20
+CALLS_BETWEEN_IMAGE = 0
 MAX_VELOCITY_ALLOWED = 30.0
 MIN_VELOCITY_ALLOWED = 1.0
 MAX_ACCELERATION_ALLOWED = 3.0  # m/s^2
@@ -50,7 +50,9 @@ i = 0
 
 def get_output(x):
     global i
-    autoEndArg = "" if i % CALLS_BETWEEN_IMAGE == 0 else "none"
+    autoEndArg = (
+        "" if CALLS_BETWEEN_IMAGE != 0 and i % CALLS_BETWEEN_IMAGE == 0 else "none"
+    )
     i += 1
     x_tuple = tuple(x)
     if x_tuple not in output_cache:
@@ -78,28 +80,28 @@ def objective(strategy_to_test):
 
     # Check energy consumption constraint
     if energy_consumption > MAX_ENERGY_CONS:
-        objective_value += abs(energy_consumption - MAX_ENERGY_CONS) * 100
+        objective_value += abs(energy_consumption - MAX_ENERGY_CONS) * 100000
 
     if energy_consumption < 0:
-        objective_value += abs(energy_consumption) * 100
+        objective_value += abs(energy_consumption) * 100000
 
     if max_velocity > MAX_VELOCITY_ALLOWED:
-        objective_value += abs(max_velocity - MAX_VELOCITY_ALLOWED) * 100
+        objective_value += abs(max_velocity - MAX_VELOCITY_ALLOWED) * 100000
 
     if min_velocity < MIN_VELOCITY_ALLOWED:
-        objective_value += abs(min_velocity) * 100
+        objective_value += abs(min_velocity) * 100000
 
     if max_acceleration > MAX_ACCELERATION_ALLOWED:
-        objective_value += abs(MAX_ACCELERATION_ALLOWED - max_acceleration) * 100
+        objective_value += abs(MAX_ACCELERATION_ALLOWED - max_acceleration) * 100000
 
     if min_acceleration < MAX_DECCELERATION_ALLOWED:
-        objective_value += abs(min_acceleration - MAX_DECCELERATION_ALLOWED) * 100
+        objective_value += abs(min_acceleration - MAX_DECCELERATION_ALLOWED) * 100000
 
     if max_centripetal_force > MAX_CENTRIPETAL_ALLOWED:
-        objective_value += abs(max_centripetal_force - MAX_CENTRIPETAL_ALLOWED) * 100
+        objective_value += abs(max_centripetal_force - MAX_CENTRIPETAL_ALLOWED) * 100000
 
     velocity_difference = abs(final_velocity - initial_velocity)
-    objective_value += velocity_difference * 100
+    objective_value += velocity_difference * 100000
 
     return (
         objective_value
@@ -113,24 +115,25 @@ expected_args = get_expected_argument_count()
 npts = 50  # Number of points in the lattice (adjust based on problem size)
 mon = VerboseMonitor(10, 50)
 
+
 lower = [0.0, -2.0]
 upper = [30.0, 3.0]
 for i in range(1, expected_args // 2):
     lower.append(-0.1)
     upper.append(0.1)
 
-    lower.append(-10.0)
-    upper.append(10.0)
+    lower.append(-30.0)
+    upper.append(30.0)
 
 # Configure and solve using LatticeSolver
-solver = LatticeSolver(expected_args)
+solver = SparsitySolver(expected_args)
 solver.SetGenerationMonitor(mon)
 solver.SetStrictRanges(lower, upper)
+solver.SetEvaluationLimits(10000000, 10000000)
+solver.SetTermination(SolverInterrupt())
 
 try:
-    solver.Solve(
-        objective, termination=ChangeOverGeneration(generations=300), disp=True
-    )
+    solver.Solve(objective, disp=True)
 except KeyboardInterrupt:
     print("\nOptimization interrupted by user.\n")
 
